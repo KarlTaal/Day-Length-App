1c4474d9e554fe6bed540fe6d2f6c768a8bdf4e0
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2020-04-17 00:28:22 +0300

    added simple map

---
+++ dayapp/static/dayapp/mapbox.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FybHRhYWwiLCJhIjoiY2s5Mzhqd2FlMDJndDNwcW54eWRtMjRmZCJ9.NPpf4n7Du1k977GiJDafEw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
